91edfa813514bea7c8b4c1e21f1bfe4a709de59c
abhi <abhijith.uwc.rw@gmail.com>
2025-01-19 01:19:25 -0500

    rotate bits without other variable

---
+++ bit_ops/rotate_bits.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+#define BIT_LEN (8)
+
+int main(void) {
+	unsigned int x = 0x28; //rotation is 0x1	
+	for(int i = 0; i< (BIT_LEN/2); i++) {
+		if(!(x & (1 << i)) && (x &(1 << ((BIT_LEN -1) - i)))) {
+			x |= (1 << i);
+			x &= ~(1 << ((BIT_LEN -1) -i));
+			continue;	
+		}
+		if((x & (1 << i)) && !(x &(1 << ((BIT_LEN -1) - i)))){
+			x &= ~(1 << i);
+			x |= (1 << ((BIT_LEN - 1) - i));
+			continue;
+		}
+			
+	}
+	printf("x is %x\n", x);
+	return 0;
+}
